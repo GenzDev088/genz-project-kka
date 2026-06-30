159a2207b8395ac6c2d53c8f07be5f6979ea5b2a
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:21:56 +0700

    Add lib/tabunganku/providers/connectivity_provider.dart from template

---
+++ lib/tabunganku/providers/connectivity_provider.dart
@@ -0,0 +1,24 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum NetworkStatus { online, offline }
+
+final networkStatusProvider = StreamProvider<NetworkStatus>((ref) async* {
+
+  final Connectivity connectivity = Connectivity();
+
+
+  final List<ConnectivityResult> initialResults = await connectivity
+      .checkConnectivity();
+
+  bool isOnline(List<ConnectivityResult> results) {
+    return results.any((r) => r != ConnectivityResult.none);
+  }
+
+  yield isOnline(initialResults) ? NetworkStatus.online : NetworkStatus.offline;
+
+
+  await for (final results in connectivity.onConnectivityChanged) {
+    yield isOnline(results) ? NetworkStatus.online : NetworkStatus.offline;
+  }
+});
