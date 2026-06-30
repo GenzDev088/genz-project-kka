a2d1fa9963c3b094707528d250be6c099fd385dc
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:16:58 +0700

    Add lib/core/app/runtimeDatas.dart from template

---
+++ lib/core/app/runtimeDatas.dart
@@ -0,0 +1,18 @@
+import 'package:otax/core/data/types.dart';
+import 'package:otax/core/database/anilist/types.dart';
+import 'package:otax/ui/theme/types.dart';
+
+
+
+UserModal? storedUserData;
+
+
+SettingsModal? currentUserSettings;
+
+
+UserPreferencesModal? userPreferences;
+
+
+late AnimeStreamTheme appTheme;
+
+late String animeOnsenToken;
