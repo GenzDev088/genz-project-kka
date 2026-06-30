d2ccd78a90ecb17ea5159c003d16143058c4c408
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:17:11 +0700

    Add lib/core/commons/subtitleParsers/subtitleParsers.dart from template

---
+++ lib/core/commons/subtitleParsers/subtitleParsers.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:otax/core/commons/subtitleParsers/assParser.dart';
+import 'package:otax/core/commons/subtitleParsers/srtParser.dart';
+import 'package:otax/core/commons/subtitleParsers/vttParser.dart';
+import 'package:otax/ui/models/widgets/subtitles/subtitle.dart';
+import 'package:http/http.dart';
+
+class Subtitleparsers {
+  Future<List<Subtitle>> parseAss(
+    String assSource, {
+    Map<String, String> headers = const {},
+  }) async {
+    if (assSource.startsWith("http")) {
+
+      final res = await get(Uri.parse(assSource), headers: headers);
+      final sub = ASSRIPPER().parseASS(utf8.decode(res.bodyBytes));
+      return sub;
+    }
+    ;
+    return ASSRIPPER().parseASS(assSource);
+  }
+
+  Future<List<Subtitle>> parseVtt(
+    String source, {
+    Map<String, String> headers = const {},
+  }) async {
+    if (source.startsWith('http')) {
+      final res = await get(Uri.parse(source), headers: headers);
+      final subs = VttRipper().parseVtt(utf8.decode(res.bodyBytes));
+      return subs;
+    }
+    return VttRipper().parseVtt(source);
+  }
+
+  Future<List<Subtitle>> parseSrt(
+    String source, {
+    Map<String, String> headers = const {},
+  }) async {
+    if (source.startsWith('http')) {
+      final res = await get(Uri.parse(source), headers: headers);
+      final subs = SrtRipper().parseSrt(utf8.decode(res.bodyBytes));
+      return subs;
+    }
+    return VttRipper().parseVtt(source);
+  }
+
+  static Duration parseDuration(String timeString) {
+    final parts = timeString.trim().split(':');
+    final hours = int.parse(parts[0]);
+    final minutes = int.parse(parts[1]);
+    final secondsParts = parts[2].split(RegExp(r'[.,]'));
+    final seconds = int.parse(secondsParts[0]);
+    final fractionStr = secondsParts.length > 1 ? secondsParts[1] : '0';
+
+    final msStr = (fractionStr.length >= 3)
+        ? fractionStr.substring(0, 3)
+        : fractionStr.padRight(3, '0');
+    final milliseconds = int.parse(msStr);
+
+    return Duration(
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+      milliseconds: milliseconds,
+    );
+  }
+}
