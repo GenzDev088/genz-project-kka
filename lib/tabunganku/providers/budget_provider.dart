843c1759c144582f8a90d4dd092fa3b876d6c77f
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:21:53 +0700

    Add lib/tabunganku/providers/budget_provider.dart from template

---
+++ lib/tabunganku/providers/budget_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:otax/tabunganku/models/budget_model.dart';
+import 'package:otax/tabunganku/services/budget_service.dart';
+
+final budgetServiceProvider = Provider<BudgetService>((ref) {
+  return MockBudgetService();
+});
+
+final budgetsStreamProvider = StreamProvider.autoDispose<List<BudgetModel>>((
+  ref,
+) {
+  final service = ref.watch(budgetServiceProvider);
+  return service.watchBudgets();
+});
+
+final currentMonthBudgetsProvider = Provider.autoDispose<List<BudgetModel>>((
+  ref,
+) {
+  final budgetsAsync = ref.watch(budgetsStreamProvider);
+  final now = DateTime.now();
+  return budgetsAsync.maybeWhen(
+    data: (data) =>
+        data.where((b) => b.month == now.month && b.year == now.year).toList(),
+    orElse: () => <BudgetModel>[],
+  );
+});
