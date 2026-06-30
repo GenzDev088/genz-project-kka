6b41a70c0886001f4af6df671e181d119bca58e0
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:16:51 +0700

    Add lib/core/anime/providers/providerPlugin.dart from template

---
+++ lib/core/anime/providers/providerPlugin.dart
@@ -0,0 +1,40 @@
+import 'package:otax/core/anime/providers/animeProvider.dart';
+
+
+class ProviderPlugin {
+  ProviderPlugin() {
+    _setupCompiler();
+  }
+
+
+
+  void _setupCompiler() {}
+
+  Future<AnimeProvider?> getProvider(
+    String provider, {
+    String? testCode,
+  }) async {
+    return null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
