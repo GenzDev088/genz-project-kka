7f4a5aab83fcbb44176d3baf8c6a2f6c26802e41
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:12:18 +0700

    Add android/app/src/main/java/com/manta/ayun/MainActivity.java from template

---
+++ android/app/src/main/java/com/manta/ayun/MainActivity.java
@@ -0,0 +1,16 @@
+package com.manta.ayun;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+
+
+
+        flutterEngine.getPlugins().add(new AppMonitorPlugin());
+    }
+}
