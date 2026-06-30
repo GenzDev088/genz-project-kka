adc0d20d82a21acfed5da708e4369eeabaf3755f
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:18:21 +0700

    Add lib/games/cyber_quest/cyber_quest.dart from template

---
+++ lib/games/cyber_quest/cyber_quest.dart
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library cyber_quest;
+
+import 'package:flutter/material.dart';
+import '../../widgets/common_widgets.dart';
+
+class CyberQuestScreen extends StatefulWidget {
+  const CyberQuestScreen({super.key});
+
+  @override
+  State<CyberQuestScreen> createState() => _CyberQuestScreenState();
+}
+
+class _CyberQuestScreenState extends State<CyberQuestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: GameAppBar(title: 'Cyber Quest'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.psychology, size: 64, color: Colors.purple),
+            SizedBox(height: 16),
+            Text(
+              'Cyber Quest',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Coming Soon!',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
