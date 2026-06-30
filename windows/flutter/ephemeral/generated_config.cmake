941434257a68456dbab46f6f7fa9e27330c3f9c7
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:25:58 +0700

    Add windows/flutter/ephemeral/generated_config.cmake from template

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\OTA\\Documents\\base MANTA" PROJECT_DIR)
+
+set(FLUTTER_VERSION "2.0.8" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 8 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\OTA\\Documents\\base MANTA"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\OTA\\Documents\\base MANTA\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\OTA\\Documents\\base MANTA"
+  "FLUTTER_TARGET=C:\\Users\\OTA\\Documents\\base MANTA\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NTU5ZmZhM2Y3NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NGM1MjVkYWM1ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\OTA\\Documents\\base MANTA\\.dart_tool\\package_config.json"
+)
