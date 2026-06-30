86613a0f79a3c107f39b8faf88fb885544fd2a96
GenzDev088 <muhamadridwan6867@gmail.com>
2026-07-01 01:22:09 +0700

    Add lib/tabunganku/providers/shopping_item_provider.dart from template

---
+++ lib/tabunganku/providers/shopping_item_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:otax/tabunganku/models/shopping_item_model.dart';
+import 'package:otax/tabunganku/services/shopping_item_service.dart';
+
+
+final shoppingItemServiceProvider = Provider<ShoppingItemService>((ref) {
+  return MockShoppingItemService();
+});
+
+
+final shoppingItemsStreamProvider =
+    StreamProvider.autoDispose<List<ShoppingItem>>((ref) {
+      final service = ref.watch(shoppingItemServiceProvider);
+      return service.watchItems();
+    });
